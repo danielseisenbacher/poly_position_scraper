16932e3fb45a9d20da9365aba156de07b4c1c56f
Daniel Seisenbacher <155956496+danielseisenbacher@users.noreply.github.com>
2026-01-10 08:54:25 +0100

    Adjust workload distribution for runners

--- 04_multi_scrape_big_fish.py
+++ 04_multi_scrape_big_fish.py
@@ -15,7 +15,7 @@ def get_big_fish(runner_index: int) -> None:
 
     new_users = list(new_users)
 
-    workload_per_runner = floor(len(new_users)/18)
+    workload_per_runner = floor(len(new_users)/20)
     if runner_index < 20:
         start_idx = workload_per_runner * runner_index
         end_idx = (
